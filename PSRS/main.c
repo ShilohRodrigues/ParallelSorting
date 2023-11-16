d0a6f7e46e9cdfd0d73ed4a3d4387aeb0e3ec12f
Shiloh Rodrigues <shilohrodrigues@hotmail.com>
2023-11-16 12:33:51 -0500

    partitioning done, needs testing, missing merging

--- PSRS/main.c
+++ PSRS/main.c
@@ -18,7 +18,7 @@ int main(int argc, char *argv[]) {
   int	  p_id,	     // process ID 
         p,         // number of processors 
         rc,        // return code 
-        i;         // for loop counter
+        i, j;         // for loop counters
 
   int *data = NULL; // Holds the data to be sorted
   int size = 0;
@@ -111,17 +111,16 @@ int main(int argc, char *argv[]) {
    * Master collects the samples and sorts them
    * Master chooses P-1 pivots at the indices P + P/2 -1, 2P + P/2 -1,…, (P-1)P + P/2 -1
    * Pivots are broadcasted to processes and each process partitions its local sorted sublists around the pivots.
+   * Each proces sends its jth partition to Pj process, and keeps its own partition Pi
+   * Each process merges partitions and combines results
    * 
   */
 
-
-  int *regular_samples = NULL;  
+  int *regular_samples = (int *)malloc(p * sizeof(int));  
   int *master_regular_samples = NULL;
-  int *pivots = NULL;
+  int *pivots = (int *)malloc((p-1) * sizeof(int));;
 
-  regular_samples = (int *)malloc(p * sizeof(int));
   if (p_id == MASTER) master_regular_samples = (int *)malloc((p * p) * sizeof(int));
-  pivots = (int *)malloc((p-1) * sizeof(int));
 
   // Choose regular samples
   for(i=0; i<p; i++) {
@@ -154,13 +153,8 @@ int main(int argc, char *argv[]) {
 
   // Send pivots
   MPI_Bcast(pivots, (p-1), MPI_INT, MASTER, MPI_COMM_WORLD);
-  
-  // Partitionnnnnn
 
- 
-  
-  
-
+  // Print the pivots for testing
   if (p_id == MASTER) {
     printf("\n"); 
     for(i = 0; i < (p-1); i++) {
@@ -168,8 +162,76 @@ int main(int argc, char *argv[]) {
     }
     printf("\n"); 
   }
+  
+  // Partition the local sublists based on the received pivots
+  int *send_counts = (int *)calloc(p, sizeof(int));
+  int *send_displs = (int *)calloc(p, sizeof(int));
+  int **send_buffers = malloc(p * sizeof(int*));
+
+  int current_pivot = 0;
+  send_buffers[0] = data_block; // first partition starts at the beginning of data_block
+
+  for (i = 0; i < block_size; i++) {
+      if (current_pivot < (p-1) && data_block[i] > pivots[current_pivot]) {
+          send_displs[current_pivot + 1] = i;
+          send_buffers[current_pivot + 1] = &data_block[i];
+          send_counts[current_pivot] = send_buffers[current_pivot + 1] - send_buffers[current_pivot];
+          current_pivot++;
+      }
+  }
+  send_counts[current_pivot] = &data_block[block_size] - send_buffers[current_pivot];
+
+  // Communicate the send_counts to all processes to determine recv_counts
+  int *recv_counts = malloc(p * sizeof(int));
+  MPI_Alltoall(send_counts, 1, MPI_INT, recv_counts, 1, MPI_INT, MPI_COMM_WORLD);
+
+  // Prepare for the all-to-all variable size data exchange
+  int total_recv = 0;
+  int *recv_displs = malloc(p * sizeof(int));
+  int *recv_data = NULL;
+
+  recv_displs[0] = 0;
+  for (i = 1; i < p; i++) {
+      recv_displs[i] = recv_displs[i - 1] + recv_counts[i - 1];
+      total_recv += recv_counts[i - 1];
+  }
+  total_recv += recv_counts[p - 1]; // add the last recv_count
+
+  recv_data = malloc(total_recv * sizeof(int));
+
+  // Now send and receive the partitions
+  MPI_Alltoallv(MPI_IN_PLACE, send_counts, send_displs, MPI_INT, 
+                recv_data, recv_counts, recv_displs, MPI_INT, 
+                MPI_COMM_WORLD);
+
+  // // The recv_data now needs to be merged, but we'll assume that's done in a separate step
+  // int *sorted_data = NULL;
+  // if (p_id == MASTER) sorted_data = (int *)malloc(size * sizeof(int));
+
+  //MPI_Gather(recv_data, recv_counts, MPI_INT, sorted_data, recv_counts, MPI_INT, MASTER, MPI_COMM_WORLD);
+
+  // if (p_id == MASTER) {
+  //   printf("\n"); 
+  //   for(i = 0; i < size; i++) {
+  //     printf("%d ", sorted_data[i]);
+  //   }
+  //   printf("\n"); 
+  // }
 
   // Clean up
+  free(send_counts);
+  free(send_displs);
+  free(recv_counts);
+  free(recv_displs);
+  free(recv_data);
+  for (i = 0; i < p; i++) {
+      if (i > 0) {
+          free(send_buffers[i]);
+      }
+  }
+  free(send_buffers);
+  free(pivots);
+  free(regular_samples);
   free(data_block);
   MPI_Finalize();
 
@@ -189,9 +251,8 @@ void swap(int* a, int* b) {
 /**
  * Partitions the array using the rightmost index
 */
-int sequential_partition(int* data, int low, int high) {
+int partition(int* data, int low, int high, int pivot) {
 
-  int pivot = data[high];
   int i = (low - 1);
   int j;
   for(j = low; j < high; j++) {
@@ -213,7 +274,7 @@ void sequential_quicksort(int* data, int low, int high) {
 
   if (low >= high) return;
 
-  int p = sequential_partition(data, low, high);
+  int p = partition(data, low, high, data[high]);
   sequential_quicksort(data, low, p-1);
   sequential_quicksort(data, p+1, high);
 
